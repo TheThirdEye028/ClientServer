c376f10ae0a42554d3e19b5f73f636e40a682de1
NaveCohen <nave.coheng@gmail.com>
2016-08-29 02:02:55 +0300

    registraion completed 95% without design

--- employee_client/js/register.service.js
+++ employee_client/js/register.service.js
@@ -1,21 +1,29 @@
 app.service('registerService', ['$http', function($http) {
     this.images = [];
+    this.posts = [];
     this.name = "";
     this.age = 0;
+    this.id = 0;
 
     this.updateImages = function(nImage) {
         this.images.push(nImage);
     };
 
-    this.updateInfo = function(name, age) {
+    this.updateInfo = function(id, name, age) {
+        this.id = id;
         this.name = name;
         this.age = age;
     };
 
+    this.updatePosts = function(posts) {
+        this.posts = posts.slice();
+    };
+
     this.postData = function() {
-        images.map(function(image) {
-            
-        });
-        return $http.post('/upload', {'name': this.name, 'age': this.age,'images': this.images});
+        return $http.post('/upload', {'id': this.id, 
+                                      'name': this.name, 
+                                      'age': this.age,
+                                      'images': this.images,
+                                      'posts': this.posts});
     };
 }]);
